6fddbdc2d02265786622d8df16aee7bcd09e76f0
Rsfabian <rsfabian1604@gmail.com>
2026-06-04 21:25:09 -0600

    se mostraron los platillos en la aplicacion

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-btnAgregarPlatillo = document.getElementById ('btnAgregarPlatillo');
+let contenido = '';
+
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
@@ -12,3 +13,27 @@ btnAgregarPlatillo.addEventListener('click', function(){
    alert('Platillo agregado');
 
 });
+
+function mostrarPlatillo(platillo, id){
+ contenido += `
+ <div class="card-panel recipe white row" data-id="${id}">
+ <div class="recipe-details">
+ <div class="recipe-title">
+      ${platillo.nombre}
+  </div>
+  <div class="recipe-ingredientes">
+      ${platillo.ingredientes}
+ </div>
+        <div class="recipe-precio">
+      ${platillo.precio}
+      
+    </div>
+  </div>
+  <div class="recipe-delete">
+      <i class="material-icons" data-id="${id}">delete_outline</i>
+  </div>
+  </div>`;
+ document.querySelector('.recipes').innerHTML = contenido;
+
+}
+
